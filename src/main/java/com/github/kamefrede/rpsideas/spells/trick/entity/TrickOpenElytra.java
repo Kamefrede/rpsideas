7e55c3c36a26ec0c7f31f08b91d966fa2fd3a7d9
Kamefrede <kamefrede@gmail.com>
2018-11-30 11:28:50 +0000

    start work on elytra tricks

---
+++ src/main/java/com/github/kamefrede/rpsideas/spells/trick/entity/TrickOpenElytra.java
@@ -0,0 +1,36 @@
+package com.github.kamefrede.rpsideas.spells.trick.entity;
+
+import vazkii.psi.api.spell.*;
+import vazkii.psi.api.spell.param.ParamNumber;
+import vazkii.psi.api.spell.piece.PieceTrick;
+
+public class TrickOpenElytra extends PieceTrick {
+
+    SpellParam num;
+
+    public TrickOpenElytra(Spell spell) {
+        super(spell);
+    }
+
+    @Override
+    public void initParams() {
+        addParam(num = new ParamNumber(SpellParam.GENERIC_NAME_NUMBER, SpellParam.RED, false, false));
+    }
+
+    @Override
+    public void addToMetadata(SpellMetadata meta) throws SpellCompilationException, ArithmeticException {
+        super.addToMetadata(meta);
+
+        meta.addStat(EnumSpellStat.COMPLEXITY, 1);
+        meta.addStat(EnumSpellStat.COST, 5);
+    }
+
+    @Override
+    public Object execute(SpellContext context) throws SpellRuntimeException {
+        double value = this.<Double>getParamValue(context, num);
+        if(Math.abs(value) < 1.0) {
+
+        }
+        return false;
+    }
+}
