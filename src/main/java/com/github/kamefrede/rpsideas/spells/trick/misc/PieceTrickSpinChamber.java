b003b85de04a170a172a33a62dc2140c3c8d6dea
Kamefrede <kamefrede@gmail.com>
2018-12-03 17:55:10 +0000

    finish up trick spin chamber

--- src/main/java/com/github/kamefrede/rpsideas/spells/trick/misc/PieceTrickSpinChamber.java
+++ src/main/java/com/github/kamefrede/rpsideas/spells/trick/misc/PieceTrickSpinChamber.java
@@ -1,8 +1,14 @@
 package com.github.kamefrede.rpsideas.spells.trick.misc;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import vazkii.psi.api.PsiAPI;
+import vazkii.psi.api.cad.EnumCADStat;
+import vazkii.psi.api.cad.ICAD;
 import vazkii.psi.api.spell.*;
 import vazkii.psi.api.spell.param.ParamNumber;
 import vazkii.psi.api.spell.piece.PieceTrick;
+import vazkii.psi.common.item.ItemCAD;
 
 public class PieceTrickSpinChamber extends PieceTrick {
 
@@ -26,6 +32,28 @@ public class PieceTrickSpinChamber extends PieceTrick {
 
     @Override
     public Object execute(SpellContext context) throws SpellRuntimeException {
-        return super.execute(context);
+        Double num = this.<Double>getParamValue(context, number);
+        if(num == null || num == 0) return null;
+        if(!(context.tool.getItem() instanceof ItemCAD)) return null;
+        EntityPlayer player = context.caster;
+        ItemStack stack = context.tool;
+        ItemCAD cad = (ItemCAD)stack.getItem();
+        if(num > 0){
+            if(cad.getSelectedSlot(stack) < cad.getStatValue(stack, EnumCADStat.SOCKETS) - 1){
+                cad.setSelectedSlot(stack, cad.getSelectedSlot(stack) + 1);
+                return true;
+            } else {
+                cad.setSelectedSlot(stack, 0);
+                return true;
+            }
+        } else {
+            if(cad.getSelectedSlot(stack) != 0){
+                cad.setSelectedSlot(stack, cad.getSelectedSlot(stack) - 1);
+                return true;
+            } else {
+                cad.setSelectedSlot(stack, cad.getStatValue(stack, EnumCADStat.SOCKETS) - 1);
+                return true;
+            }
+        }
     }
 }
