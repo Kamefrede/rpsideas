2aeda7bd6e4a00c6f9f6e7f6dbcd7a28ec596e95
Kamefrede <kamefrede@gmail.com>
2018-12-08 19:43:29 +0000

    version bump

--- src/main/java/com/github/kamefrede/rpsideas/util/Reference.java
+++ src/main/java/com/github/kamefrede/rpsideas/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MODID = "rpsideas";
     public static final String NAME = "Random PSIdeas";
-    public static final String VERSION = "1.11c";
+    public static final String VERSION = "1.11d";
     public static final String CLIENT_PROXY_CLASS = "com.github.kamefrede.rpsideas.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.github.kamefrede.rpsideas.proxy.CommonProxy";
 
