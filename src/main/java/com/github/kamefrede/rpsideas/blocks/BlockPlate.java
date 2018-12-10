3231342a79227a7986bf6316b8febdb82dfbdd22
Kamefrede <kamefrede@gmail.com>
2018-12-10 23:25:29 +0000

    finish up the colored plates

--- src/main/java/com/github/kamefrede/rpsideas/blocks/BlockPlate.java
+++ src/main/java/com/github/kamefrede/rpsideas/blocks/BlockPlate.java
@@ -7,6 +7,7 @@ import com.github.kamefrede.rpsideas.util.libs.LibItems;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
@@ -15,6 +16,7 @@ import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.relauncher.Side;
@@ -23,19 +25,20 @@ import vazkii.arl.block.BlockMetaVariants;
 import vazkii.arl.block.BlockMod;
 import vazkii.arl.interf.IBlockColorProvider;
 
-public class BlockPlate extends BlockMetaVariants implements IBlockColorProvider {
+public class BlockPlate extends BlockMod implements IBlockColorProvider {
 
     public static class Companion{
-        public void makeVariants(){
+        public static String[] makeVariants(String name) {
             String[] array = new String[16];
             for (int i = 0; i < array.length; i++)
-                array[i] = LibBlocks.PLATE_BLOCK + LibItems.colors.get(i);
+                array[i] = name + "_" + EnumDyeColor.byMetadata(i).getName();
+            return array;
         }
 
 
     }
     public BlockPlate(String name){
-        super(name, Material.IRON, EnumDyeColor.class);
+        super(name, Material.IRON, Companion.makeVariants(name));
         setLightLevel(1f);
         this.setHardness(5.0f);
         this.setResistance(10.0f);
@@ -46,22 +49,28 @@ public class BlockPlate extends BlockMetaVariants implements IBlockColorProvider
     public static final PropertyEnum<EnumDyeColor> COLOR = PropertyEnum.create("color", EnumDyeColor.class);
 
 
+    @Override
     protected BlockStateContainer createBlockState() {
         return new BlockStateContainer(this, COLOR);
     }
 
+    @Override
+    public String[] getVariants() {
+        return super.getVariants();
+    }
 
-
+    @Override
     public int getMetaFromState(IBlockState state) {
         return state.getValue(COLOR).getMetadata();
     }
 
-
+    @Override
     public IBlockState getStateFromMeta(int meta) {
         return getDefaultState().withProperty(COLOR, EnumDyeColor.byMetadata(meta));
     }
 
 
+    @Override
     public int damageDropped(IBlockState state) {
         return getMetaFromState(state);
     }
@@ -71,6 +80,8 @@ public class BlockPlate extends BlockMetaVariants implements IBlockColorProvider
         return MapColor.getBlockColor(state.getValue(COLOR));
     }
 
+
+
     @SideOnly(Side.CLIENT)
     @Override
     public BlockRenderLayer getRenderLayer() {
@@ -79,7 +90,7 @@ public class BlockPlate extends BlockMetaVariants implements IBlockColorProvider
 
     @Override
     public IBlockColor getBlockColor() {
-        return (IBlockState iBlockState, IBlockAccess access, BlockPos pos, int i) -> i == 1 ? MapColor.getBlockColor(iBlockState.getValue(COLOR)).colorValue : 0xFFFFFF;
+        return (IBlockState iBlockState, IBlockAccess access, BlockPos pos, int i) -> i == 1 ? MapColor.getBlockColor(iBlockState.getValue(COLOR)).colorValue  : 0xFFFFFF;
     }
 
     @Override
@@ -91,6 +102,7 @@ public class BlockPlate extends BlockMetaVariants implements IBlockColorProvider
     public String getModNamespace() {
         return Reference.MODID;
     }
+
 
 
 }
--- src/main/java/com/github/kamefrede/rpsideas/util/libs/LibBlocks.java
+++ src/main/java/com/github/kamefrede/rpsideas/util/libs/LibBlocks.java
@@ -7,9 +7,8 @@ public class LibBlocks {
     public final static String CONJURED_PULSAR_BLOCK = "conjured_pulsar_block";
     public static final String CONJURED_STAR_BLOCK = "conjured_star_block";
     public static final String PLATE_BLOCK = "block_plate";
-    public static final String BRIGHT_PLATE = "brightPlate";
-    public static final String DARK_PLATE = "darkPlate";
-    public static final String PLATE_BASE = "platebase";
+    public static final String BRIGHT_PLATE = "bright_plate";
+    public static final String DARK_PLATE = "dark_plate";
     
 
 
