92a9c364d0a8a1b45eaf860a8f33b309e5034e48
yrsegal@gmail.com <yrsegal@gmail.com>
2019-05-17 18:41:05 -0400

    better glowing selector. This now has a use!

--- src/main/java/com/kamefrede/rpsideas/spells/selector/PieceSelectorGlowing.java
+++ src/main/java/com/kamefrede/rpsideas/spells/selector/PieceSelectorGlowing.java
@@ -2,7 +2,7 @@ package com.kamefrede.rpsideas.spells.selector;
 
 import com.google.common.base.Predicate;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityEnderEye;
 import net.minecraft.init.MobEffects;
 import vazkii.psi.api.spell.Spell;
@@ -17,8 +17,7 @@ public class PieceSelectorGlowing extends PieceSelectorNearby {
     @Override
     @SuppressWarnings("Guava")
     public Predicate<Entity> getTargetPredicate() {
-        return (Entity e) -> (e instanceof EntityLiving && ((EntityLiving) e).isPotionActive(MobEffects.GLOWING)) ||
-                e instanceof EntityEnderEye ||
-                (e != null && e.isBurning());
+        return (Entity e) -> e != null && (e instanceof EntityEnderEye || e.isBurning() || e.isGlowing() ||
+                (e instanceof EntityLivingBase && ((EntityLivingBase) e).isPotionActive(MobEffects.GLOWING)));
     }
 }
