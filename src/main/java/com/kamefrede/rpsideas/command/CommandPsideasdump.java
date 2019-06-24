bfac4fa5873fea4729c63796f60a81be3aff7909
Kamefrede <kamefrede@gmail.com>
2019-06-24 13:23:59 +0100

    Forgot to annotate these

--- src/main/java/com/kamefrede/rpsideas/command/CommandPsideasdump.java
+++ src/main/java/com/kamefrede/rpsideas/command/CommandPsideasdump.java
@@ -8,21 +8,25 @@ import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
 import org.apache.logging.log4j.Level;
+import org.jetbrains.annotations.NotNull;
 import vazkii.psi.api.PsiAPI;
 
 public class CommandPsideasdump extends CommandBase {
+
+    @NotNull
     @Override
     public String getName() {
         return "psideas-dump";
     }
 
+    @NotNull
     @Override
-    public String getUsage(ICommandSender sender) {
+    public String getUsage(@NotNull ICommandSender sender) {
         return localizationKey() + ".usage";
     }
 
     @Override
-    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+    public void execute(@NotNull MinecraftServer server, ICommandSender sender, @NotNull String[] args) throws CommandException {
         sender.sendMessage(new TextComponentString("Dumping registry names to the log file!"));
         sender.sendMessage(new TextComponentString("Beware that any disabled pieces on the config will not show up in the output"));
         for (String s : PsiAPI.spellPieceRegistry.getKeys()) {
