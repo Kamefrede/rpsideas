bd50995e006fae263c319bea540b27a423cdb470
Kamefrede <kamefrede@gmail.com>
2019-06-24 13:23:29 +0100

    Small tweak so that blacklisted pieces don't show up in the output

--- src/main/java/com/kamefrede/rpsideas/command/CommandPsideasdump.java
+++ src/main/java/com/kamefrede/rpsideas/command/CommandPsideasdump.java
@@ -1,6 +1,7 @@
 package com.kamefrede.rpsideas.command;
 
 import com.kamefrede.rpsideas.RPSIdeas;
+import com.kamefrede.rpsideas.util.RPSConfigHandler;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
@@ -23,8 +24,9 @@ public class CommandPsideasdump extends CommandBase {
     @Override
     public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
         sender.sendMessage(new TextComponentString("Dumping registry names to the log file!"));
+        sender.sendMessage(new TextComponentString("Beware that any disabled pieces on the config will not show up in the output"));
         for (String s : PsiAPI.spellPieceRegistry.getKeys()) {
-            if (s.startsWith("rpsideas"))
+            if (s.startsWith("rpsideas") && !RPSConfigHandler.isPieceBlacklisted(s.substring(s.indexOf(".") + 1)))
                 RPSIdeas.LOGGER.log(Level.INFO, s.substring(s.indexOf(".") + 1));
         }
         sender.sendMessage(new TextComponentString("Done logging!"));
